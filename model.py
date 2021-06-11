7ea9ec3a1ccd2038ecda28d1c857a63733ce448b
Abhiram Gurijala <abhiram.gurijala@gmail.com>
2021-06-11 22:36:30 +0530

    model.py with  Receptive Fields and comments

--- model.py
+++ model.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# # Python program to create a CNN model with Normalization to detect MNIST digits
+
+# ### Import the required libraries
+
 # In[1]:
 
 
@@ -15,7 +19,7 @@ import os
 
 # # Model Architecture
 
-# ## Batch Normalization
+# ## Class for Model with Batch Normalization
 
 # In[2]:
 
@@ -32,29 +36,35 @@ class BatchNormalization(nn.Module):
 
         # Input Block
         self.conv1 = nn.Sequential(
+            #input - RF:1x1, Channel Size: 28x28; Output RF: 3x3, Channel Size: 26x26
             nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3, 3), padding=0, bias=False), 
             nn.ReLU(),
-            nn.BatchNorm2d(8)
+            nn.BatchNorm2d(8) 
         )
         
         # CONVOLUTION BLOCK 1
         self.conv2 = nn.Sequential(
+            #input - RF:3x3, Channel Size: 26x26; Output RF: 5x5, Channel Size: 24x24
             nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU(),
-            nn.BatchNorm2d(16)
+            nn.BatchNorm2d(16) 
         )
 
         # TRANSITION BLOCK 1
         self.trans = nn.Sequential(
+            #input - RF:5x5, Channel Size: 24x24; Output RF: 5x5, Channel Size: 24x24
             nn.Conv2d(in_channels=16, out_channels=8, kernel_size=(1, 1), padding=0, bias=False),
-            nn.MaxPool2d(2, 2)
+            #input - RF:5x5, Channel Size: 24x24; Output RF: 6x6, Channel Size: 12x12
+            nn.MaxPool2d(2, 2) 
         )
         
         # CONVOLUTION BLOCK 2
         self.conv3 = nn.Sequential(
+            #input - RF:6x6, Channel Size: 12x12; Output RF: 10x10, Channel Size: 10x10
             nn.Conv2d(in_channels=8, out_channels=12, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU(),            
             nn.BatchNorm2d(12),
+            #input - RF:10x10, Channel Size: 10x10; Output RF: 14x14, Channel Size: 8x8
             nn.Conv2d(in_channels=12, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU(),            
             nn.BatchNorm2d(16),
@@ -62,6 +72,7 @@ class BatchNormalization(nn.Module):
         )
 
         self.conv4 = nn.Sequential(
+            #input - RF:14x14, Channel Size: 8x8; Output RF: 18x18, Channel Size: 6x6
             nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU(),            
             nn.BatchNorm2d(16),
@@ -69,6 +80,7 @@ class BatchNormalization(nn.Module):
         )
 
         self.conv5 = nn.Sequential(
+            #input - RF:18x18, Channel Size: 6x6; Output RF: 22x22, Channel Size: 6x6
             nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), padding=1, bias=False),
             nn.ReLU(),            
             nn.BatchNorm2d(16),
@@ -76,10 +88,12 @@ class BatchNormalization(nn.Module):
 
         # OUTPUT BLOCK
         self.gap = nn.Sequential(
-            nn.AvgPool2d(kernel_size=6)
+            #input - RF:22x22, Channel Size: 6x6; Output RF: 32x32, Channel Size: 1x1
+            nn.AvgPool2d(kernel_size=6) 
         )
 
         self.conv6 = nn.Sequential(
+            #input - RF:32x32, Channel Size: 1x1; Output RF: 32x32, Channel Size: 1x1
             nn.Conv2d(in_channels=16, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
         ) 
 
@@ -97,17 +111,20 @@ class BatchNormalization(nn.Module):
         return F.log_softmax(x, dim=-1)
 
 
-# ## Group Normalization
+# ## Class for Model with Group Normalization
 
 # In[4]:
 
-
+# In Group Normalization, we forms groups of channels in each layer,
+# This will result in channels of each group to be normalized at once.
+# Here, we use nn.GroupNorm(num_groups,num_channels) after each Conv3d layer to implement Group Normalization.
 class GroupNormalization(nn.Module):
     def __init__(self):
         super(GroupNormalization, self).__init__()
 
         # Input Block
         self.conv1 = nn.Sequential(
+            #input - RF:1x1, Channel Size: 28x28; Output RF: 3x3, Channel Size: 26x26
             nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3, 3), padding=0, bias=False), 
             nn.ReLU(),
             nn.GroupNorm(num_groups=2, num_channels=8)
@@ -115,6 +132,7 @@ class GroupNormalization(nn.Module):
         
         # CONVOLUTION BLOCK 1
         self.conv2 = nn.Sequential(
+            #input - RF:3x3, Channel Size: 26x26; Output RF: 5x5, Channel Size: 24x24
             nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU(),
             nn.GroupNorm(num_groups=4, num_channels=16)
@@ -122,15 +140,19 @@ class GroupNormalization(nn.Module):
 
         # TRANSITION BLOCK 1
         self.trans = nn.Sequential(
+            #input - RF:5x5, Channel Size: 24x24; Output RF: 5x5, Channel Size: 24x24
             nn.Conv2d(in_channels=16, out_channels=8, kernel_size=(1, 1), padding=0, bias=False),
+            #input - RF:5x5, Channel Size: 24x24; Output RF: 6x6, Channel Size: 12x12
             nn.MaxPool2d(2, 2)
         )
         
         # CONVOLUTION BLOCK 2
         self.conv3 = nn.Sequential(
+            #input - RF:6x6, Channel Size: 12x12; Output RF: 10x10, Channel Size: 10x10 
             nn.Conv2d(in_channels=8, out_channels=12, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU(),            
             nn.GroupNorm(num_groups=3, num_channels=12),
+            #input - RF:10x10, Channel Size: 10x10; Output RF: 14x14, Channel Size: 8x8
             nn.Conv2d(in_channels=12, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU(),            
             nn.GroupNorm(num_groups=4, num_channels=16),
@@ -138,6 +160,7 @@ class GroupNormalization(nn.Module):
         )
 
         self.conv4 = nn.Sequential(
+            #input - RF:14x14, Channel Size: 8x8; Output RF: 18x18, Channel Size: 6x6
             nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU(),            
             nn.GroupNorm(num_groups=4, num_channels=16),
@@ -145,6 +168,7 @@ class GroupNormalization(nn.Module):
         )
 
         self.conv5 = nn.Sequential(
+            #input - RF:18x18, Channel Size: 6x6; Output RF: 22x22, Channel Size: 6x6
             nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), padding=1, bias=False),
             nn.ReLU(),            
             nn.GroupNorm(num_groups=4, num_channels=16),
@@ -152,10 +176,12 @@ class GroupNormalization(nn.Module):
 
         # OUTPUT BLOCK
         self.gap = nn.Sequential(
+            #input - RF:22x22, Channel Size: 6x6; Output RF: 32x32, Channel Size: 1x1
             nn.AvgPool2d(kernel_size=6)
         )
 
         self.conv6 = nn.Sequential(
+            #input - RF:32x32, Channel Size: 1x1; Output RF: 32x32, Channel Size: 1x1
             nn.Conv2d(in_channels=16, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
         ) 
 
@@ -173,65 +199,85 @@ class GroupNormalization(nn.Module):
         return F.log_softmax(x, dim=-1)
 
 
-# ## Layer Normalization
+# ## Class for Model with Layer Normalization
 
 # In[5]:
 
-
+# Layer Normalization is a special case of Group Normalization wherein we selct the group count as 1
+# This will result in the entire channels in the layer to be normalized at once.
+# Here, we use nn.GroupNorm(1,num_channels) after each Conv3d layer to implement Layer Normalization.
 class LayerNormalization(nn.Module):
     def __init__(self):
         super(LayerNormalization, self).__init__()
 
         # Input Block
         self.conv1 = nn.Sequential(
+            #input - RF:1x1, Channel Size: 28x28; Output RF: 3x3, Channel Size: 26x26
             nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3, 3), padding=0, bias=False), 
             nn.ReLU(),
+            # Layer Normalization
             nn.GroupNorm(1,8)
         )
         
         # CONVOLUTION BLOCK 1
         self.conv2 = nn.Sequential(
+            #input - RF:3x3, Channel Size: 26x26; Output RF: 5x5, Channel Size: 24x24
             nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU(),
+            # Layer Normalization
             nn.GroupNorm(1,16)
         )
 
         # TRANSITION BLOCK 1
         self.trans = nn.Sequential(
+            #input - RF:5x5, Channel Size: 24x24; Output RF: 5x5, Channel Size: 24x24
             nn.Conv2d(in_channels=16, out_channels=8, kernel_size=(1, 1), padding=0, bias=False),
+            #input - RF:5x5, Channel Size: 24x24; Output RF: 6x6, Channel Size: 12x12
             nn.MaxPool2d(2, 2)
         )
         
         # CONVOLUTION BLOCK 2
         self.conv3 = nn.Sequential(
+            #input - RF:6x6, Channel Size: 12x12; Output RF: 10x10, Channel Size: 10x10 
             nn.Conv2d(in_channels=8, out_channels=12, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU(),            
+            # Layer Normalization
             nn.GroupNorm(1,12),
+            #input - RF:10x10, Channel Size: 10x10; Output RF: 14x14, Channel Size: 8x8
             nn.Conv2d(in_channels=12, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU(),            
+            # Layer Normalization
             nn.GroupNorm(1,16),
             nn.Dropout(DROPOUT_VALUE)
         )
-
+        
+        # CONVOLUTION BLOCK 3
         self.conv4 = nn.Sequential(
+            #input - RF:14x14, Channel Size: 8x8; Output RF: 18x18, Channel Size: 6x6
             nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU(),            
+            # Layer Normalization
             nn.GroupNorm(1,16),
             nn.Dropout(DROPOUT_VALUE)
         )
 
+        # CONVOLUTION BLOCK 4
         self.conv5 = nn.Sequential(
+            #input - RF:18x18, Channel Size: 6x6; Output RF: 22x22, Channel Size: 6x6
             nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), padding=1, bias=False),
             nn.ReLU(),            
+            # Layer Normalization
             nn.GroupNorm(1,16),
         )
 
         # OUTPUT BLOCK
         self.gap = nn.Sequential(
+            #input - RF:22x22, Channel Size: 6x6; Output RF: 32x32, Channel Size: 1x1
             nn.AvgPool2d(kernel_size=6)
         )
 
         self.conv6 = nn.Sequential(
+            #input - RF:32x32, Channel Size: 1x1; Output RF: 32x32, Channel Size: 1x1
             nn.Conv2d(in_channels=16, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
         ) 
 
@@ -249,18 +295,25 @@ class LayerNormalization(nn.Module):
         return F.log_softmax(x, dim=-1)
 
 
+# ## build_model: This function accepts type of Normalization as input and returns the model object with corresponding normalization applied
+# ##              Raises an exception in case of an incorrect Normalization type is given in input
 # In[18]:
 
 
 def build_model(model_type):
+    # Check if the input is Batch Normalization
     if model_type == 'BN':
         model = BatchNormalization()
+    # Check if the input is Group Normalization
     elif model_type == 'GN':
         model = GroupNormalization()
+    # Check if the input is Layer Normalization
     elif model_type == 'LN':
         model = LayerNormalization()
+    # Raise an exception in case the input is invalid
     else:
         raise Exception("Invalid Normalization: model_type should be 'BN' or 'GN' or 'LN'")
     
+    # Return the model object to the calling program
     return model
 
